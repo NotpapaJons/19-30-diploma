50b6d8b1989ec915e9e9048e589c834a60215f52
NotpapaJons <marcenkopavel56@gmail.com>
2025-04-20 17:24:46 +0700

    123

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        title="Галерея",
+    )
+
+@app.route('/about')
+def about():
+
+    return render_template('about.html', title="о нас")
 
 app.run(debug=True)
